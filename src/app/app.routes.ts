5320719dfa45692f2cc2cb81f871881b41430161
ThiagoSuchi <thiagohenssuchi@gmail.com>
2026-05-10 14:55:26 -0400

    feat(app): add default routes

---
+++ src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/pages/home-page/home-page.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+];
