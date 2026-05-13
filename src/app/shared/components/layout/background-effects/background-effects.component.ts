0d7b26bb7a44b3c4f0e2eaf59190d858c319d98b
ThiagoSuchi <thiagohenssuchi@gmail.com>
2026-05-13 19:37:43 -0400

    feat(layout): add background effects component class

---
+++ src/app/shared/components/layout/background-effects/background-effects.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-background-effects',
+  standalone: true,
+  templateUrl: './background-effects.component.html',
+  styles: []
+})
+export class BackgroundEffectsComponent {}
