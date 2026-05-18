7b5f05ac0178f0e77e5810b3a4ccb02f648a5b66
ThiagoSuchi <thiagohenssuchi@gmail.com>
2026-05-18 19:05:32 -0400

    feat: add live vote counter animation to live-results component

--- src/app/features/home/components/live-results/live-results.component.ts
+++ src/app/features/home/components/live-results/live-results.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SectionHeadingComponent } from '../../../../shared/components/ui/section-heading/section-heading.component';
 
@@ -17,21 +17,69 @@ interface Result {
   templateUrl: './live-results.component.html',
   styles: []
 })
-export class LiveResultsComponent {
+export class LiveResultsComponent implements OnInit, OnDestroy {
   results: Result[] = [
     {
       name: 'Brasil',
       flag: '🇧🇷',
       votes: 15420,
-      percentage: 65,
+      percentage: 65.0,
       color: 'bg-frederico-yellow',
     },
     {
       name: 'Argentina',
       flag: '🇦🇷',
       votes: 8304,
-      percentage: 35,
+      percentage: 35.0,
       color: 'bg-frederico-red',
     },
   ];
+
+  totalVotes: number = 0;
+  leaderText: string = 'BRASIL ASSUME A LIDERANÇA';
+  private updateInterval: any;
+
+  ngOnInit() {
+    this.calculateTotals();
+    
+    // Simula a contagem animada na tela para passar vibe de transmissão "Ao Vivo"
+    this.updateInterval = setInterval(() => {
+      // Votos aleatórios simulados
+      const plusBr = Math.floor(Math.random() * 5);
+      const plusAr = Math.floor(Math.random() * 3);
+      
+      this.results[0].votes += plusBr;
+      this.results[1].votes += plusAr;
+      
+      this.calculateTotals();
+    }, 2500);
+  }
+
+  ngOnDestroy() {
+    if (this.updateInterval) {
+      clearInterval(this.updateInterval);
+    }
+  }
+
+  calculateTotals() {
+    this.totalVotes = this.results.reduce((acc, curr) => acc + curr.votes, 0);
+    
+    this.results.forEach(res => {
+      res.percentage = Number(((res.votes / this.totalVotes) * 100).toFixed(1));
+    });
+
+    const isBrasilLeading = this.results[0].votes > this.results[1].votes;
+    
+    if (Math.abs(this.results[0].percentage - this.results[1].percentage) < 5) {
+      this.leaderText = 'DISPUTA ACIRRADA! VOTO A VOTO';
+    } else {
+      this.leaderText = isBrasilLeading 
+        ? 'BRASIL MANTÉM FORTE LIDERANÇA' 
+        : 'ARGENTINA ASSUME A LIDERANÇA';
+    }
+  }
+
+  getLeader(): Result {
+    return this.results[0].votes > this.results[1].votes ? this.results[0] : this.results[1];
+  }
 }
